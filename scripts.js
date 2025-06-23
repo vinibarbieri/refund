09cbbab5f38ee1423c24a69e2a16c7f95babf7af
vinibarbieri <vinicius190702@hotmail.com>
2025-06-23 20:31:37 -0300

    feat: create-`newExpense`-object

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const expense = document.getElementById('expense');
+const category = document.getElementById('category');
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, '');
@@ -8,10 +11,25 @@ amount.oninput = () => {
 
 function formatCurrencyUSD(value) {
     const valueInCents = value / 100
-    const valueFormated = valueInCents.toLocaleString('en-US', {
+    const valueInUSD = valueInCents.toLocaleString('en-US', {
         style: 'currency',
         currency: 'USD',
     })
 
-    return valueFormated;
+    return valueInUSD;
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    console.log(newExpense);
 }
