8b5c95bc9cae1b9c55e348020fc18f4a3f72d2b7
vinibarbieri <vinicius190702@hotmail.com>
2025-06-23 20:13:00 -0300

    feat: format-to-USD-currency

--- scripts.js
+++ scripts.js
@@ -3,5 +3,15 @@ const amount = document.getElementById('amount');
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, '');
 
-    amount.value = value;
+    amount.value = formatCurrencyUSD(value);
+}
+
+function formatCurrencyUSD(value) {
+    const valueInCents = value / 100
+    const valueFormated = valueInCents.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    })
+
+    return valueFormated;
 }
